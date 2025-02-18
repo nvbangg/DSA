c27782e5884c8bfc6f98afe5e39758ad392ea33c
nvbangg <nvbangg@gmail.com>
2025-02-18 09:54:48 +0700

    b3

---
+++ BT tren lop/chuong 1/bai 4.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<pair<int, int>> thua_so(int n)
+{
+    vector<pair<int, int>> res;
+    for (int i = 2; i <= sqrt(n); ++i)
+    {
+        int cnt = 0;
+        while (n % i == 0)
+        {
+            cnt++;
+            n /= i;
+        }
+        if (cnt > 0)
+            res.push_back({i, cnt});
+    }
+    if (n > 1)
+        res.push_back({n, 1});
+    return res;
+}
+void TestCase()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int>> res = thua_so(n);
+    for (int i = 0; i < res.size(); ++i)
+        cout << res[i].first << " (" << res[i].second << ")";
+    cout << endl;
+}
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+        TestCase();
+    return 0;
+}
