dea42c190d945547098d42988593ffe7846c9d0f
nvbangg <nvbangg@gmail.com>
2025-03-04 09:37:28 +0700

    refactor selection sort to use a function and return sorted array

--- BT tren lop/C2_Sap xep va Tim kiem/selection sort.cpp
+++ BT tren lop/C2_Sap xep va Tim kiem/selection sort.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+vector<int> selection_sort(vector<int> a)
+{
+    int n = a.size();
+    for (int i = 0; i < n; ++i)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (a[j] < a[i])
+                min = j;
+        }
+        swap(a[i], a[min]);
+    }
+    return a;
+}
 int main()
 {
     int n;
@@ -8,17 +22,7 @@ int main()
     vector<int> a(n);
     for (int &x : a)
         cin >> x;
-    for (int i = 0; i < n; ++i)
-    {
-        int min = i;
-        for (int j = i + 1; j < n; ++j)
-        {
-            if (a[j] <= a[i])
-                min = j;
-        }
-        swap(a[i], a[min]);
-        for (auto x : a)
-            cout << x << " ";
-        cout << endl;
-    }
+    a = selection_sort(a);
+    for (int x : a)
+        cout << x << " ";
 }
