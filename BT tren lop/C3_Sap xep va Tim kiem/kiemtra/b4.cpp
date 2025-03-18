eb6bdfbe42f855885407e29f381e011c940c345f
nvbangg <nvbangg@gmail.com>
2025-03-18 10:59:48 +0700

    edit

--- BT tren lop/C3_Sap xep va Tim kiem/kiemtra/b4.cpp
+++ BT tren lop/C3_Sap xep va Tim kiem/kiemtra/b4.cpp
@@ -7,12 +7,11 @@ void TestCase()
     vector<int> a(n);
     for (int &x : a)
         cin >> x;
+    sort(a.begin(), a.end());
+    long long ans = 0;
     for (int i = 0; i < n; ++i)
-        if (a[i] == k)
-        {
-            cout << i + 1 << endl;
-            return;
-        }
+        ans += lower_bound(a.begin() + i + 1, a.end(), a[i] + k) - (a.begin() + i + 1);
+    cout << ans << endl;
 }
 int main()
 {
